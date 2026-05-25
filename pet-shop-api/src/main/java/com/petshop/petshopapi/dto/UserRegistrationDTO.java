f03be4b61f60992efe4cb38e2530cc3d0e28040d
Luiz Nunes <luizfmn.dev@gmail.com>
2026-05-25 09:18:36 -0300

    feat(dto): add UserRegistrationDTO and UserResponseDTO

---
+++ pet-shop-api/src/main/java/com/petshop/petshopapi/dto/UserRegistrationDTO.java
@@ -0,0 +1,22 @@
+package com.petshop.petshopapi.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserRegistrationDTO (
+
+        @NotBlank(message = "Name cannot be blank")
+        String name,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, message = "Password must be at least 8 characters long")
+        String password,
+
+        @NotBlank(message = "Role is required")
+        String role
+) {}
---
+++ pet-shop-api/src/main/java/com/petshop/petshopapi/dto/UserResponseDTO.java
@@ -0,0 +1,4 @@
+package com.petshop.petshopapi.dto;
+
+public record UserResponseDTO (Long id, String name, String email, String role) {
+}
