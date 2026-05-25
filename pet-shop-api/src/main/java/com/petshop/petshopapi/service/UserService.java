fa81db10b04356dfb98814eb1dbf5487b775efed
Luiz Nunes <luizfmn.dev@gmail.com>
2026-05-25 09:25:49 -0300

    feat(user): add UserService for user registration, update repository with email existence check

---
+++ pet-shop-api/src/main/java/com/petshop/petshopapi/service/UserService.java
@@ -0,0 +1,35 @@
+package com.petshop.petshopapi.service;
+
+import com.petshop.petshopapi.dto.UserRegistrationDTO;
+import com.petshop.petshopapi.dto.UserResponseDTO;
+import com.petshop.petshopapi.entity.User;
+import com.petshop.petshopapi.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserResponseDTO registerUser(UserRegistrationDTO userRegistrationDTO) {
+        if (userRepository.existsByEmail(userRegistrationDTO.email())) {
+            throw new RuntimeException("Email is already in use");
+        }
+
+        User user = new User();
+        user.setName(userRegistrationDTO.name());
+        user.setEmail(userRegistrationDTO.email());
+        user.setRole(userRegistrationDTO.role());
+        user.setPassword(passwordEncoder.encode(userRegistrationDTO.password()));
+
+        userRepository.save(user);
+
+        return new UserResponseDTO(user.getId(), user.getName(), user.getEmail(), user.getRole());
+    }
+}
