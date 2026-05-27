5c603e92e8068b36a10604646561adcb5876dc7f
Luiz Nunes <luizfmn.dev@gmail.com>
2026-05-27 10:11:13 -0300

    fix: extend AbstractIntegrationTest in default application tests

--- pet-shop-api/src/test/java/com/petshop/petshopapi/PetShopApiApplicationTests.java
+++ pet-shop-api/src/test/java/com/petshop/petshopapi/PetShopApiApplicationTests.java
@@ -1,10 +1,11 @@
 package com.petshop.petshopapi;
 
+import com.petshop.petshopapi.config.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PetShopApiApplicationTests {
+class PetShopApiApplicationTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {
