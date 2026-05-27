4f244aa53f5eb99ae5d7ea236cae79a42e148b76
Luiz Nunes <luizfmn.dev@gmail.com>
2026-05-27 14:05:09 -0300

    feat(auth): create UserLoginDTO and skeleton for AuthService

---
+++ pet-shop-api/src/main/java/com/petshop/petshopapi/dto/UserLoginDTO.java
@@ -0,0 +1,15 @@
+package com.petshop.petshopapi.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserLoginDTO(
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        String password
+
+) {}
---
+++ pet-shop-api/src/main/java/com/petshop/petshopapi/service/AuthService.java
@@ -0,0 +1,13 @@
+package com.petshop.petshopapi.service;
+
+import com.petshop.petshopapi.dto.UserLoginDTO;
+import com.petshop.petshopapi.dto.UserResponseDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    public UserResponseDTO login(UserLoginDTO userLoginDTO) {
+        return null;
+    }
+}
