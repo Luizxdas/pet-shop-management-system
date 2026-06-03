967c73c2b335fa9c82fa246094bc1fd3e50cc3ac
Luiz Nunes <luizfmn.dev@gmail.com>
2026-06-03 12:18:19 -0300

    feat(pet): implement pet controller for registration

---
+++ pet-shop-api/src/main/java/com/petshop/petshopapi/controller/PetController.java
@@ -0,0 +1,34 @@
+package com.petshop.petshopapi.controller;
+
+import com.petshop.petshopapi.dto.pet.PetRegistrationDTO;
+import com.petshop.petshopapi.dto.pet.PetResponseDTO;
+import com.petshop.petshopapi.service.PetService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/pets")
+public class PetController {
+
+    PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<PetResponseDTO> register(@Valid @RequestBody PetRegistrationDTO dto, Authentication authentication) {
+        String loggedInEmail = authentication.getName();
+
+        PetResponseDTO petResponseDTO = petService.registerPet(dto, loggedInEmail);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(petResponseDTO);
+    }
+
+}
